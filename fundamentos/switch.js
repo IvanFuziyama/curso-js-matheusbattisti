08043b2e5aec04bd4d4748faa0508ff4ad182c2b
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-10-06 19:24:33 -0300

    switch

---
+++ fundamentos/switch.js
@@ -0,0 +1,16 @@
+//switch
+const fruta = "banana"
+switch(fruta){
+    case "banana":
+        console.log("banana é a fruta")
+        break
+    case "laranja":
+        console.log("laranja é a fruta")
+        break
+    case "maçã":
+        console.log("maçã é a fruta")
+        break
+    default:
+        console.log("sem fruta")
+        break
+}
