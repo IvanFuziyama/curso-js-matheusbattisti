8028f1afa0a18ffcd45d0e86a428dbcb5930750b
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-10-14 22:26:25 -0300

    lista compra

---
+++ exercicios-avancados/ex001.js
@@ -0,0 +1,10 @@
+//ex
+//crie uma lista de compras que é inicialmente vazia.
+//adicione 5 itens à lista usando push()
+//agora,remova o primeiro item da lista usando shift()
+//imprima a lista final no console
+
+const lista = [ ]
+lista.push('banana', 'maçã', 'melão', 'melancia', 'laranja')
+lista.shift()
+console.log(lista)
